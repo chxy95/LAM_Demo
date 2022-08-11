a9b951c3376f1c88e05204c954aabf9929cee50d
chxy95 <824534882@qq.com>
2022-08-11 21:58:01 +0800

    test lam

--- swin_lam.py
+++ swin_lam.py
@@ -12,16 +12,17 @@ from SaliencyModel.BackProp import GaussianBlurPath
 from SaliencyModel.utils import grad_norm, IG_baseline, interpolation, isotropic_gaussian_kernel
 
 model_name = 'SwinIR'
-window_size = 40 # 80  # Define windoes_size of D
-image_path = './test_images/Belmondo.png'
-w = 11  # The x coordinate of your select patch, 125 as an example
-h = 18  # The y coordinate of your select patch, 160 as an example
+window_size = 20 # 80  # Define windoes_size of D
+image_path = './test_images/lam.png'
+w = 120  # The x coordinate of your select patch, 125 as an example
+h = 118  # The y coordinate of your select patch, 160 as an example
         # And check the red box
         # Is your selected patch this one? If not, adjust the `w` and `h`.
 image_name = os.path.basename(image_path)[:-4]
 idx = 1
 
-for model_variants in ['SRx4_win8', 'SRx4_win16', 'SRx4_win32']:
+# for model_variants in ['SRx4_win8', 'SRx4_win16', 'SRx4_win32']:
+for model_variants in ['SRx4_win8', 'SRx4_win16']:
     model = load_model(model_name + '@' + model_variants)
 
     img_lr, img_hr = prepare_images(image_path)  # Change this image name
@@ -49,5 +50,8 @@ for model_variants in ['SRx4_win8', 'SRx4_win16', 'SRx4_win32']:
         blend_kde_and_input,
         Tensor2PIL(torch.clamp(torch.from_numpy(result), min=0., max=1.))]
     )
-
-    pil.save('./Results/' + '_'.join([model_name, model_variants, image_name, str(idx)]) + '.png')
+    
+    gini_index = gini(abs_normed_grad_numpy)
+    diffusion_index = (1 - gini_index) * 100
+    print(f"The DI of this case is {diffusion_index}")
+    # pil.save('./Results/' + '_'.join([model_name, model_variants, image_name, str(idx)]) + '.png')
